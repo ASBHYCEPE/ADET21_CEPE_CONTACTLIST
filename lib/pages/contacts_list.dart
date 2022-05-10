6bda5e4d1812bb0bfbb3b6f1a11296fd79f334a4
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-10 22:05:06 +0800

    Added a list builder for displaying a list of contatcs

--- lib/pages/contacts_list.dart
+++ lib/pages/contacts_list.dart
@@ -1,3 +1,6 @@
+import 'package:contact_list/models/contacts.dart';
+import 'package:contact_list/services/database_helper.dart';
+import 'package:contact_list/widget/contact_card.dart';
 import 'package:flutter/material.dart';
 import 'package:contact_list/pages/contact_details.dart';
 
@@ -22,7 +25,27 @@ class _ContactListState extends State<ContactList> {
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-        child: Container(),
+        child: FutureBuilder(
+          future: DatabaseHelper.instance.getAllContacts(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Contact>?> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else if (snapshot.hasData) {
+              if (snapshot.data != null) {
+                ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) =>
+                        ContactCard(contact: snapshot.data![index]));
+              }
+            }
+            return const Center(
+              child: Text('NO CONTACTS'),
+            );
+          },
+        ),
       ),
     );
   }
