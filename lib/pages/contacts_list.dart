7de7640356442f6a927080344d97ae82d43c3dd3
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-10 20:22:11 +0800

    Added code for routing to contact details page

--- lib/pages/contacts_list.dart
+++ lib/pages/contacts_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:contact_list/pages/contact_details.dart';
 
 class ContactList extends StatefulWidget {
   ContactList({Key? key}) : super(key: key);
@@ -12,13 +13,16 @@ class _ContactListState extends State<ContactList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('CONTACT LIST')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ContactDetails()));
+        },
+        child: const Icon(Icons.person_add),
+      ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
         child: Container(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.person_add),
       ),
     );
   }
