a5b3a80628d1f665389c4c45ae47d5472e14118d
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-10 21:03:16 +0800

    Added a function for initializing database

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -1,1 +1,18 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class DatabaseHelper {
+  static const int _version = 1;
+  static const String _dbName = 'Contacts.db';
+  static Database? _database;
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  DatabaseHelper._init();
+
+  Future<Database> _initDB(String dbName) async {
+    final dbFilePath = await getDatabasesPath();
+    final path = join(dbFilePath, dbName);
+
+    return await openDatabase(path, version: _version);
+  }
+}
